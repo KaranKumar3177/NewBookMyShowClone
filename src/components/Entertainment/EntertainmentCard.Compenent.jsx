0d6b571172e5b0b83f85d5010c14296270834db9
KaranKumar3177 <57917748+KaranKumar3177@users.noreply.github.com>
2024-06-14 00:23:56 +0000

    work start

--- src/components/Entertainment/EntertainmentCard.Compenent.jsx
+++ src/components/Entertainment/EntertainmentCard.Compenent.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Slider} from "react-slick";
+
+const EntertainmentCard = (props) =>{
+    return (
+        <>
+        <div className='w-full h-30 px-2'>
+            <img src={props.src} alt='entertainment' className='w-full h-full rounded-lg' />
+             </div>
+        </>
+    )
+}
 
 const EntertainmentCardSlider = () => {
+    const EntertainmentImage = [];
+
+    const settings = {};
+
   return (
-    <div>EntertainmentCard</div>
+   <>
+   <Slider {...settings}>
+    {EntertainmentImage.map((image)=> (
+        <EntertainmentCard src={image} />
+    ))}
+    </Slider> 
+   </>
   )
 }
 
--- src/components/Navbar/Navbar.Component.jsx
+++ src/components/Navbar/Navbar.Component.jsx
@@ -35,7 +35,15 @@ function NavLg(){
                 </div>
                 <div className='w-full flex items-center gap-3 bg-white px-3 py-1 rounded-md'>
                     <BiSearch />
-                    {/* <input type="search" className=''/> */}
+                    { <input type="search" className='w-full bg-transparent border-none focus:outline-none'
+                    placeholder='Search for movies, event, plays, sports and activities'/> }
+                </div>
+            </div>
+            <div className='flex items-center gap-3'>
+                <span className='text-gray-200 text-base flex items-center cursor-pointer hover:text-white'>Bangalore <BiChevronDown/></span>
+                <button className='bg-red-600 text-white px-2 py-1 text-sm rounded'>Sign In</button>
+                <div className='w-8 h-8 text-white'>
+                    <BiMenu className='w-full h-full'/>
                 </div>
             </div>
         </div>
@@ -45,7 +53,22 @@ function NavLg(){
 // Main Component
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='bg-darkBackground-700 px-4 py-3'>
+        {/*Mobile Screen*/}
+        <div className='md:hidden'>
+            <NavSm/>
+            </div>
+
+             {/*Medium Screen*/}
+        <div className='hidden md:flex lg:hidden'>
+            <NavMd/>
+            </div>
+
+             {/*Large Screen*/}
+        <div className='hidden md:hidden lg:flex'>
+            <NavLg/>
+            </div>
+    </nav>
   )
 }
 
